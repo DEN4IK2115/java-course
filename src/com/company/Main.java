59506f76677576cd567253a05e28f2c49bbd5f4e
DEN4IK2115 <kozlovdenis21@mail.ru>
2019-08-13 11:38:46 +0300

    Задание №1.0 выполнено Задача:
    Заполните массив случайным числами и выведете максимальное, минимальное и среднее значение.
    
    Для генерации случайного числа используйте метод Math.random(), который возвращает значение в промежутке [0, 1].

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int n = 100;
+        double []array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random() * 100;
+        }
+        double max = array[0];
+        double min = array[0];
+        double avg = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(max < array[i])
+                max = array[i];
+            else {
+                if (min > array[i])
+                    min = array[i];
+            }
+            avg += array[i];
+        }
+        avg = avg/ array.length;
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+        System.out.println("avg = " + avg);
+
     }
 }
